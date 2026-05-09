3ea21effe7a1b6efcbbf02c3529b58981163fae6
devendra1176 <devendra1176sahu@gmail.com>
2026-05-09 08:08:41 +0530

    solve: add pattern_6 solution

---
+++ Pattern/Pattern_6.java
@@ -0,0 +1,18 @@
+// Given an integer n. You need to recreate the pattern given below for any value of N.
+// Let's say for N = 5, the pattern should look like as below:
+// 12345
+// 1234
+// 123
+// 12
+// 1
+public class Pattern_6 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
