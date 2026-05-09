99453851a73e11ea3c9f912cdcbfc240dfb6a6ee
devendra1176 <devendra1176sahu@gmail.com>
2026-05-09 07:26:58 +0530

    solve:add Pattern_4 solution

---
+++ Pattern/Pattern_4.java
@@ -0,0 +1,20 @@
+// Given an integer n. You need to recreate the pattern given below for any value of N. 
+// Let's say for N = 5, the pattern should look like as below:
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+class Pattern_4 {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i=1; i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(i);
+            }
+            System.out.println("");
+        }
+    }
+}
